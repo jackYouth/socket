cbe8246fcfde610c4bd7e8f55308488165609f6a
suyd@ctrip.com <suyd@ctrip.com>
2018-12-04 18:10:37 +0800

    init_depository

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  //   res.sendFile(__dirname + "/index.html");
+  res.send({ test: 111 });
+});
+
+io.on("connection", socket => {
+  console.log("a user connection");
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listen on *:3000");
+});
