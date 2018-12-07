2eeaaaadd4e6bade65b67de380410740249db560
suyd@ctrip.com <suyd@ctrip.com>
2018-12-07 10:45:51 +0800

    study

--- 剪刀石头布/server/app.ts
+++ 剪刀石头布/server/app.ts
@@ -1,7 +1,7 @@
-import * as express from 'express';
-import * as path from 'path';
-import * as logger from 'morgan';
-import * as bodyParser from 'body-parser';
+import * as express from "express";
+import * as path from "path";
+import * as logger from "morgan";
+import * as bodyParser from "body-parser";
 import { Server } from "./game/server";
 import { config } from "./config";
 
