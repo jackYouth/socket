907003d91f0b3071b8d8ce0f872b4c8ea0ff1588
suyd@ctrip.com <suyd@ctrip.com>
2018-12-27 20:55:59 +0800

    add_dist

---
+++ 剪刀石头布/server/dist/config.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    rootPath: "/applet/websocket/socket.io"
+};
+//# sourceMappingURL=config.js.map
