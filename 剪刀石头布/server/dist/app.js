e2d928ec40d9972a11154823f6c8c940bd76c0b9
jackyouth <1849552082@qq.com>
2018-12-27 23:34:26 +0800

    update

--- 剪刀石头布/server/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./game/server");
-const config_1 = require("./config");
-var server = new server_1.Server();
-server.init(config_1.config.rootPath);
-server.start();
-//# sourceMappingURL=app.js.map
--- 剪刀石头布/server/dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    rootPath: "/applet/websocket/socket.io"
-};
-//# sourceMappingURL=config.js.map
